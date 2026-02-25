3413493e25d4c4ab84abfb7aaa75a60903f19fcd
grupodvbr <grupodvbr@gmail.com>
2026-02-25 11:06:03 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'grupo-dv-pwa-v1';
+
+const FILES = [
+  './app.html',
+  './manifest.json',
+  './leo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => k !== CACHE_NAME && caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
